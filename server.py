7690f33674c2b1e59d67e4e19ae73d2acda58c79
Maxim <maxim.maltsev@recogizer.de>
2018-09-27 15:02:15 +0200

    improved router performance and added comments

--- server.py
+++ server.py
@@ -1,55 +1,66 @@
-# https://gist.github.com/mdonkers/63e115cc0c79b4f6b8b3a6b797e485c7C
-# https://stackoverflow.com/questions/1094185/how-to-serve-any-file-type-with-pythons-basehttprequesthandler
-# Python 3.6
+# -*- coding: utf-8 -*-
+"""A module for running a tiny web server.
+
+First, constants are introduced, such as host and port of the server and list of endpoints.
+Then, a functionality for the routing of comming requests is implemented.
+After that, a class for the request handling is introduced.
+At last, a method for running the server is described.
+All functionality is implemented in Python 3.6.
+"""
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
+import endpoints
 
-
-def config():
-    with open('config.json') as f:
-        data = json.load(f)
-        return data['HOST'], data['PORT'], data['ENDPOINTS']
-
-
-HOST, PORT, ENDPOINTS = config()
+# Global constants initialization #
+HOST, PORT, ENDPOINTS = endpoints.CONFIG['HOST'], endpoints.CONFIG['PORT'], endpoints.CONFIG['ENDPOINTS']
 
 
 def router(path, request_type, post_data=''):
-    # if path in ENDPOINTS and request_type == 'POST' or request_type == ENDPOINTS[path]['request_type']:
-    if path == '/example':
-        f = open('example.json', 'rb')
-        return f.read(), 'application/json'
-    return (request_type + ' request for {}'.format(path)).encode('utf-8'), 'text/html'
+    """Route requests."""
+    if path in ENDPOINTS:
+        if request_type == 'GET' and ENDPOINTS[path]['request_type'] == 'POST':
+            return endpoints.client_error()
+        endpoint_method_name = path.replace('/', '_')
+        endpoint_method = getattr(endpoints, endpoint_method_name)
+        return endpoint_method()
+    else:
+        return endpoints.static(path)
+    return endpoints.server_error()
 
 
 class S(BaseHTTPRequestHandler):
-    def _set_response(self, mime_type='text/html'):
-        self.send_response(200)
+    """Handle requests."""
+
+    def _set_response(self, mime_type, status_code):
+        """Send response and headers."""
+        self.send_response(status_code)
         self.send_header('Content-type', mime_type)
         self.end_headers()
 
     def do_GET(self):
-        response_data, mime_type = router(self.path, 'GET')
-        self._set_response(mime_type)
+        """Handle GET requests."""
+        response_data, mime_type, status_code = router(self.path, 'GET')
+        self._set_response(mime_type, status_code)
         self.wfile.write(response_data)
 
     def do_POST(self):
+        """Handle POST requests."""
         content_length = int(self.headers['Content-Length'])  # size of data
         post_data = self.rfile.read(content_length)  # data itself
-        response_data, mime_type = router(self.path, 'POST', post_data)
-        self._set_response(mime_type)
+        response_data, mime_type, status_code = router(self.path, 'POST', post_data)
+        self._set_response(mime_type, status_code)
         self.wfile.write(response_data)
 
 
-def run(server_class=HTTPServer, handler_class=S):
-    server_address = (HOST, PORT)
+def run(host=HOST, port=PORT, server_class=HTTPServer, handler_class=S):
+    """Execute web server."""
+    server_address = (host, port)
     httpd = server_class(server_address, handler_class)
     try:
-        print('Tiny Web Server is up at {}:{}'.format(HOST, PORT))
+        print('Tiny Web Server is up at {}:{}'.format(host, port))
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
     httpd.server_close()
 
 
-run()
+run(HOST, PORT)
